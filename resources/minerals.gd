2673dfd642f0aa9deaa79e8365f613e76f6226be
TheodoreBillotte <theodore.billotte@epitech.eu>
2025-07-28 21:21:47 +0200

    add: implement mineral resources and inventory system

---
+++ resources/minerals.gd
@@ -0,0 +1,23 @@
+extends Resource
+
+class_name Minerals
+
+@export var wood: int
+@export var coal: int
+@export var stone: int
+@export var copper: int
+@export var amethyst: int
+
+func add(minerals: Minerals):
+	wood += minerals.wood
+	coal += minerals.coal
+	stone += minerals.stone
+	copper += minerals.copper
+	amethyst += minerals.amethyst
+
+func subtract(minerals: Minerals):
+	wood -= minerals.wood
+	coal -= minerals.coal
+	stone -= minerals.stone
+	copper -= minerals.copper
+	amethyst -= minerals.amethyst
