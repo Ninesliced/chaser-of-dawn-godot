fa70ca9961e4e9ac4295cfd827688cd815216c60
Alexis Belmonte <alexbelm48@gmail.com>
2025-07-28 23:18:42 +0200

    update: implement `border_touched`

--- scenes/menu/play_explain.gd
+++ scenes/menu/play_explain.gd
@@ -3,8 +3,8 @@ class_name PlayExplain
 @export var scene_to_load: PackedScene
 
 func _input(event: InputEvent) -> void:
-    print(visible)
-    if !visible:
-        return
-    if event.is_action_pressed("c"):
-        get_tree().change_scene_to_packed(scene_to_load)
+	print(visible)
+	if !visible:
+		return
+	if event.is_action_pressed("c"):
+		get_tree().change_scene_to_packed(scene_to_load)
