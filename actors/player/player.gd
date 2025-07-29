4e8cb61fb8001cf96fb7e1300f6ec62ab5e5f88e
Alexis Belmonte <alexbelm48@gmail.com>
2025-07-29 20:33:33 +0200

    update: handle camera movement for world shadows shader

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -4,15 +4,15 @@ class_name Player
 @export var speed: float = 200.0
 
 func _ready() -> void:
-    pass
+	pass
 
 func _physics_process(delta: float) -> void:
-    pass
+	pass
 
 func _process(delta: float) -> void:
-    pass
+	pass
 
 
 func _on_animation_component_on_die_animation_finished() -> void:
-    Global.game_over()
-    pass # Replace with function body.
+	Global.game_over()
+	pass # Replace with function body.
